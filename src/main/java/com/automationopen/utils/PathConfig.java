3aa1c66e2215c12d4f524ea1893effbc9aa75051
iAmNani7 <naveenkumar.podilli@gmail.com>
2024-07-23 20:49:22 +0530

    initial

---
+++ src/main/java/com/automationopen/utils/PathConfig.java
@@ -0,0 +1,10 @@
+package com.automationopen.utils;
+
+public class PathConfig {
+    private String url="https://o2web-qa-01.kartrocket.in/";
+
+    public String getUrl() {
+        return  this.url=url;
+    }
+
+}
