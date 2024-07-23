435b7ae5dfe90e33ec8188346516da8570e175c9
iAmNani7 <naveenkumar.podilli@gmail.com>
2024-07-23 20:44:46 +0530

    initial commit

---
+++ src/test/java/com/automationopen/tests/Login.java
@@ -0,0 +1,13 @@
+package com.automationopen.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+        public static void main(String[] args) {
+            WebDriver driver=new ChromeDriver();
+            driver.get("https://www.google.com");
+
+    }
+}
